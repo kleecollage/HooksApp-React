7923e9af7796fd3d8613495422dfad35bbbfed83
kleeCollage <klee.collage@gmail.com>
2026-03-04 16:44:12 -0600

    feat: Implement useRef and connection between multiple customHooks

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
+import { FocusScreen } from './04-userRef/FocusScreen'
+// import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
 // import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
 // import { TrafficLight } from './01-useSate/TrafficLight'
 // import { HooksApp } from './HooksApp'
+// import { PokemonPage } from './03-examples/PokemonPage'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
@@ -11,6 +13,8 @@ createRoot(document.getElementById('root')!).render(
     {/* <HooksApp /> */}
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithEffect /> */}
-    <TrafficLightWithHook />
+    {/* <TrafficLightWithHook /> */}
+    {/* <PokemonPage /> */}
+    <FocusScreen />
   </StrictMode>,
 )
