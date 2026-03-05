2bfb4f3928e5f5011ac4f01cc0d7fe7993f028a5
Antonio Hernandez Santander <klee.collage@gmail.com>
2026-03-05 13:17:11 -0600

    feat: Implement logic on useStates and handleClicks

--- src/05-useReducer/TasksApp.tsx
+++ src/05-useReducer/TasksApp.tsx
@@ -18,23 +18,39 @@ export const TasksApp = () => {
   const [inputValue, setInputValue] = useState('');
 
   const addTodo = () => {
-    console.log('Agregar tarea', inputValue);
+    if (inputValue.length === 0) return;
 
+    const newTodo: Todo = {
+      id: Date.now(),
+      text: inputValue.trim(),
+      completed: false
+    }
+
+    setTodos([...todos, newTodo])
+    setInputValue('');
   };
 
   const toggleTodo = (id: number) => {
     console.log('Cambiar de true a false', id);
+    const updatedTodos = todos.map( (todo) => {
+      if (todo.id === id)
+        return {...todo, completed: !todo.completed }
 
-  };
+      return todo;
+    });
+    setTodos(updatedTodos);
+  }
 
   const deleteTodo = (id: number) => {
     console.log('Eliminar tarea', id);
-
+    const updatedTodo = todos.filter(todos => todos.id !== id);
+    setTodos(updatedTodo);
   };
 
   const handleKeyPress = (e: React.KeyboardEvent) => {
-    console.log('Presiono enter');
-
+    if(e.key === 'Enter') {
+      addTodo();
+    }
   };
 
   const completedCount = todos.filter((todo) => todo.completed).length;
